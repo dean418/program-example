1d4353fa945b8252692a69aa5623d68a966e1367
Dean <deansingleton1999@gmail.com>
2025-02-28 02:03:47 +0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+void print_another_msg() {
+  printf("something else");
+}
+
 void print_msg() {
   printf("something else");
 }
@@ -8,5 +12,6 @@ int main() {
   printf("hello world!\n");
   printf("bcdefgh");
   print_msg();
+  print_another_msg();
   return 0;
 }
